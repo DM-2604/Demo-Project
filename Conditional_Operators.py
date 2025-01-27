9e92e85b6d561355d7a047e2192de1d23e440d8f
DM-2604 <dmjs2608@gmail.com>
2025-01-27 10:42:46 +0530

    Add files via upload

---
+++ Conditional_Operators.py
@@ -0,0 +1,25 @@
+#Condition Operator
+#Example : if,else,elif
+
+a=50
+print("A=",a,"\n")
+#1. if condition
+print("If condition ")
+if a==50:
+    print(" A equal to  50")
+
+#2. if else condition
+print("\nIf else condition")
+if a>90:
+    print(" A is grater than 50")
+else:
+    print(" A is smaller than 90")
+
+#3. if elif else condition
+print("\nIf elif else condition")
+if a>90:
+    print(" A is grater than 50")
+elif a==90:
+    print(" A is equal to 90")
+else:
+    print(" A is smaller than 90")
