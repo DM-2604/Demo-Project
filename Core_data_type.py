34128f1da33d8aeb39ca570bd44338d5236a9e8d
DM-2604 <dmjs2608@gmail.com>
2025-01-27 10:09:21 +0530

    Add files via upload

---
+++ Core_data_type.py
@@ -0,0 +1,23 @@
+#core data type
+#python can detect automatic data type
+#1. int 
+d1=5
+print(type(d1))
+#2. float
+float_type=12.5
+print(type(float_type))
+#3. string
+string_type="i"
+print(type(string_type))
+#4. tuple 
+tuple_type=(1,3,4,2)
+print(type(tuple_type))
+#5. list 
+list_type=[1,3,4,2]
+print(type(list_type))
+#6. set 
+set_type={1,3,4,2}
+print(type(set_type))
+#7. dictonary  
+dict_type={"Name":"Maruti","Price":150000,}
+print(type(dict_type))
---
+++ print_and_outpput.py
@@ -0,0 +1,3 @@
+#Print and Input function 
+a=input("Enter any word or number ")
+print(a)
